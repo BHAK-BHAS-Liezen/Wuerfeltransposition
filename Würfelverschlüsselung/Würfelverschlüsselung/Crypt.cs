34bee24f535b27f0e6d5421ce7905186534090e2
Schuasta <77636023+Schuasta@users.noreply.github.com>
2022-11-11 23:36:54 +0100

    Add files via upload

---
+++ Würfelverschlüsselung/Würfelverschlüsselung/Crypt.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Würfelverschlüsselung
+{
+    internal class Crypt
+    {
+        public Crypt() { }
+
+         public string Decrypt(string klartext)
+        {
+            var fulltext = klartext.Replace(" ", "").ToUpper();
+            fulltext.ToUpper();
+
+            Console.WriteLine("Wie lang soll der Zeichenblock sein");
+
+            var is_int = int.TryParse(Console.ReadLine(), out int length);
+            
+            if (is_int)
+            {
+                var height = fulltext.Length / length;
+                int p = 0;
+                char[,] charArr = new char[height, length];
+                for (int i = 0; i < height; i++)
+                {
+
+                    for (int l = 0; l < length; l++)
+                    {
+                        charArr[i, l] = fulltext[l + p];
+                        //Console.Write(charArr[i, l]);
+                    }
+                    //Console.WriteLine();
+                    p = p + length;
+                }
+                //for(int i = 0; i < height; i++)
+                //{
+
+                //    for (int l = 0; l < length; l++)
+                //    {
+                //        norm[i, l] = charArr[l, i];
+
+
+                //    }
+                char[,] crypt = new char[length, height];
+                string cyphertext="";
+                for (int i = 0; i < length; i++)
+                {
+                    for (int l = 0; l < height; l++)
+                    {
+                        crypt[i, l] = charArr[l, i];
+                        cyphertext=cyphertext+crypt[i,l];
+                    }
+                }
+                Console.WriteLine("Die Verschlüsselung lautet");
+                Console.WriteLine(cyphertext);
+                return cyphertext;
+            }
+            else
+            {
+                Console.WriteLine("Falsche Eingabe");
+                return klartext;
+            }
+           
+        }
+        public  string entcrypt(string crypttext)
+        {
+            crypttext.ToUpper();
+
+            Console.WriteLine("Wie lang soll der Zeichenblock sein");
+
+            var is_int = int.TryParse(Console.ReadLine(), out int length);
+            
+            if (is_int)
+            {
+                var height = crypttext.Length / length;
+                int p = 0;
+                char[,] charArr = new char[length, height];
+                for (int i = 0; i < length; i++)
+                {
+
+                    for (int l = 0; l < height; l++)
+                    {
+                        charArr[i, l] = crypttext[l+p];
+                        //Console.Write(charArr[i, l]);
+                    }
+                    //Console.WriteLine();
+                    p = p + height;
+                }
+
+                char[,] norm = new char[height, length];
+                string text="";
+                for (int i = 0; i < height; i++)
+                {
+
+                    for (int l = 0; l < length; l++)
+                    {
+                        norm[i, l] = charArr[l, i];
+                        text = text + norm[i, l];
+                        
+                        
+                    }
+            
+                }
+                Console.WriteLine("Der Text Lautet");
+                Console.WriteLine(text);
+                return text;
+                
+                
+            }
+            else
+            {
+                Console.WriteLine("Ein fehler ist aufgetreten bitte versuchen sie es nocheinmal");
+                return crypttext;
+            }
+        }
+    }
+}
---
+++ Würfelverschlüsselung/Würfelverschlüsselung/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Würfelverschlüsselung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\Dominik\Documents\Projects\Würfelverschlüsselung\";
+            var source = path + @"Text.txt";
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("Möchtest du einen Eigenen Text eingeben (N) oder ein Dokument ve/entrschlüsseln (D)");
+                var input = Console.ReadLine();
+                if (input.ToUpper() == "N")
+                {
+                    Console.WriteLine("wollen sie den Text ent-(E) oder verschlüsseln (V)");
+                    var act = Console.ReadLine();
+                    if (act.ToUpper() == "V")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Bitte geben sie einen Text ein");
+                        string new_text = Console.ReadLine();
+                        var cypher = new Crypt();
+                        cypher.Decrypt(new_text);
+                    }
+                    if (act.ToUpper() == "E")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Bitte geben sie den Cyphertext ein");
+                        var cyphertext = Console.ReadLine();
+                        var decypher = new Crypt();
+                        decypher.entcrypt(cyphertext);
+
+                    }
+                }
+                if (input.ToUpper() == "D")
+                {
+                    Console.WriteLine("wollen sie den Text ent-(E) oder verschlüsseln (V)");
+                    var act = Console.ReadLine();
+                    if (act.ToUpper() == "V")
+                    {
+
+                        var sr= new StreamReader(source);
+                        var normtext= sr.ReadToEnd();
+                        sr.Close();
+                        var cypher = new Crypt();
+                        string cyphertext = cypher.Decrypt(normtext);
+                        var sw= new StreamWriter(source);
+                        sw.WriteLine(cyphertext);
+                        sw.Close();
+                    }
+                    if (act.ToUpper() == "E")
+                    {
+                        var sr = new StreamReader(source);
+                        var cyphertext = sr.ReadToEnd();
+                        sr.Close();
+                        var cypher = new Crypt();
+                        string klartext = cypher.entcrypt(cyphertext);
+                        var sw = new StreamWriter(source);
+                        sw.WriteLine(klartext);
+                        sw.Close();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Zum beenden drücken Sie (P)");
+                }
+                } while (Console.ReadKey(true).Key != ConsoleKey.P) ;
+            
+        }
+    }
+}
